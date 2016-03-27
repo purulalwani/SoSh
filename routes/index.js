92efc64da473d0d4f4c720191277aa7b99d99b14
rooma27 <rooma.roopchandani@gmail.com>
2016-03-27 17:52:13 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -599,7 +599,7 @@ router.get('/ratetransaction', function(req, res, next) {
     	        if(err){ console.log(err); return next(err); }
     	        });
     		// var json = '{ "_id" : "56f5a041ee3c7948433d7273", "customerId": "88882522", "txnReferenceNo":"1111111111", "merchantId":"amozon", "totalAmount":"20000.00", shareText:"view purchase <a href=\"http://host:port/viewshare?txnrefno=1111111111\" value=\"http://ss.co/1111111111\"></a>","rating":3,"views":1,"items":[{"itemName":"iPhone 5s", "itemPrice":"17000"}, {"itemName":"iPhone 5s Case", "itemPrice":"3000"}]}';
-    	        res.end(data);
+    	        res.json(data);
     		
         }else{
         	 var json = '{ "_id" : "56f5a041ee3c7948433d7273", "customerId": "88882522", "txnReferenceNo":"1111111111", "merchantId":"amozon", "totalAmount":"20000.00", shareText:"view purchase <a href=\"http://host:port/viewshare?txnrefno=1111111111\" value=\"http://ss.co/1111111111\"></a>","rating":3,"views":1,"items":[{"itemName":"iPhone 5s", "itemPrice":"17000"}, {"itemName":"iPhone 5s Case", "itemPrice":"3000"}]}';
